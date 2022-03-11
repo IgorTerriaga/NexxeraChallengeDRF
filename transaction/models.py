d47c19a47d086cee7059448c852dec7b759c6f03
Igor Terriaga <igsantos1996@gmail.com>
2022-03-10 23:24:48 -0300

    formatando data para padrão BR e exibindo o tipo o serializerMethodField

--- transaction/models.py
+++ transaction/models.py
@@ -4,17 +4,20 @@ from django.db import models
 class Conta(models.Model):
     titular = models.CharField(max_length=43)
     saldo = models.FloatField(default=0)
-    data_abertura = models.DateField()
+    data_abertura = models.DateField("%d-%m-%Y")
 
     def __str__(self):
         return self.titular
+
+    def get_data_abertura(self, obj):
+        return obj.data_abertura.strftime("%d-%m-%Y")
 
 
 class Transacao(models.Model):
     TIPO = ("C", "Credito"), ("D", "Debito")
     discriminacao = models.CharField(max_length=100)
     valor = models.FloatField()
-    data = models.DateField()
+    data = models.DateField(format("%d-%m-%Y"))
     status = models.CharField(max_length=12)
     saldo_inicial = models.FloatField()
     saldo_final = models.FloatField()
@@ -23,3 +26,6 @@ class Transacao(models.Model):
 
     def __str__(self):
         return self.discriminacao
+    
+    def get_data(self, obj):
+        return obj.data.strftime("%d-%m-%Y")
--- transaction/serializer.py
+++ transaction/serializer.py
@@ -2,18 +2,53 @@ from rest_framework import serializers
 
 from transaction.models import Conta, Transacao
 
+
 class ContaSerializer(serializers.ModelSerializer):
+    data_abertura = serializers.DateField(format("%d-%m-%Y"))
+
     class Meta:
         model = Conta
-        fields = ['id', 'titular', 'saldo', 'data_abertura']
+        fields = ["id", "titular", "saldo", "data_abertura"]
 
 
 class TransacaoSerializer(serializers.ModelSerializer):
+    data = serializers.DateField(format("%d-%m-%Y"))
+    tipo = serializers.SerializerMethodField()
     class Meta:
         model = Transacao
-        fields = '__all__'
+        fields = [
+            "id",
+            "discriminacao",
+            "valor",
+            "data",
+            "status",
+            "saldo_inicial",
+            "saldo_final",
+            "conta",
+            "tipo",
+        ]
+    def get_tipo(self, obj):
+        return obj.get_tipo_display()
+
+
 
 class ListContaTransacoesSerializer(serializers.ModelSerializer):
+    tipo = serializers.SerializerMethodField()
+    data = serializers.DateField(format("%d-%m-%Y"))
+
     class Meta:
         model = Transacao
-        fields = '__all__'
+        fields = [
+            "id",
+            "discriminacao",
+            "valor",
+            "data",
+            "status",
+            "saldo_inicial",
+            "saldo_final",
+            "conta",
+            "tipo",
+        ]
+
+    def get_tipo(self, obj):
+        return obj.get_tipo_display()
